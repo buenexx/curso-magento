c5a8ddbd2e9964b821175cdfd4b16b5bfe60cc53
Fernando Bueno <buenofernando2013@gmail.com>
2020-05-29 11:54:16 -0300

    continuar da aula 35

---
+++ app/code/Magenteiro/PromoTops/Block/TesteBlock.php
@@ -0,0 +1,11 @@
+<?php
+
+ namespace Magenteiro\PromoTops\Block;
+
+ class TesteBlock extends \Magento\Framework\View\Element\Template
+ {
+     public function getTest()
+     {
+         return 'test works';
+     }
+ }
